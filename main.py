b77a88e295ec01ed40f04ed03c2096aa9ecb7615
Antonio Herazo <antonioherazo7@hotmail.com>
2021-08-06 20:30:26 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import random
+from tkinter import *
+import pandas
+
+BACKGROUND_COLOR = "#B1DDC6"
+word = ''
+timer = None
+current_card = {}
+to_learn_dict = {}
+
+# ------------------------------LOGIC----------------------------------------
+
+try:
+    data = pandas.read_csv('./data/words_to_learn.csv')
+except FileNotFoundError:
+    original_data = pandas.read_csv('data/french_words.csv')
+    to_learn_dict = original_data.to_dict(orient='records')
+else:
+    to_learn_dict = data.to_dict(orient='records')
+
+
+def new_card():
+    global flip_timer
+    global current_card
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn_dict)
+    canvas.itemconfig(card_title, text='French', fill='black')
+    canvas.itemconfig(card_word, text=f'{current_card["French"]}', fill='black')
+    canvas.itemconfig(canvas_image, image=card_front_img)
+    flip_timer = window.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text='English', fill='white')
+    canvas.itemconfig(card_word, text=f'{current_card["English"]}', fill='white')
+    canvas.itemconfig(canvas_image, image=card_back_img)
+
+
+def is_known():
+    to_learn_dict.remove(current_card)
+    data = pandas.DataFrame(to_learn_dict)
+    data.to_csv('data/words_to_learn.csv', index=False)
+    new_card()
+
+
+# ------------------ UI SETUP --------------------------
+
+window = Tk()
+window.title('Flash Card Language Learning')
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, flip_card)
+
+canvas = Canvas(height=526, width=800)
+card_front_img = PhotoImage(file='./images/card_front.png')
+card_back_img = PhotoImage(file='./images/card_back.png')
+canvas_image = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text='', font=('Arial', 40, 'italic'))
+card_word = canvas.create_text(400, 263, text='', font=('Arial', 60, 'bold'))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(column=0, row=0, columnspan=2)
+
+cross_image = PhotoImage(file='images/wrong.png')
+unknown_button = Button(image=cross_image, highlightthickness=0, command=new_card)
+unknown_button.grid(column=0, row=1)
+
+check_image = PhotoImage(file='images/right.png')
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(column=1, row=1)
+
+new_card()
+
+window.mainloop()
